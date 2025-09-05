39dbb0ce40eaa79a86aac4dfa17792cb1237b4f9
Vadym <vlisnychuk@daltonstate.edu>
2025-09-04 20:31:11 -0400

    transfers

---
+++ Programming Assignment 3/Part 2/Supply.java
@@ -0,0 +1,58 @@
+public class Supply {
+    private int vertical;
+
+    public Supply(int vertical) {
+        this.vertical = vertical;
+    }
+
+    public int getVertical() {
+        return vertical;
+    }
+
+    static class Facade {
+        private int bat;
+        private int fix;
+
+        Facade(int bat, int fix) {
+            this.bat = bat;
+            this.fix = fix;
+        }
+
+        public int getBat() {
+            return bat;
+        }
+
+        public int getFix() {
+            return fix;
+        }
+
+        @Override
+        public String toString() {
+            return "Bat: " + bat + "\n" + "Fix: " + fix;
+        }
+    }
+
+    class Claim {
+        private int cat;
+        private int awl;
+
+        public Claim(int cat, int awl) {
+            this.cat = cat;
+            this.awl = awl;
+        }
+
+        public int getCat() {
+            return cat;
+        }
+
+        public int getAwl() {
+            return awl;
+        }
+
+        @Override
+        public String toString() {
+            return "Cat: " + cat + "\n" + "Awl: " + awl;
+        }
+
+    }
+}
